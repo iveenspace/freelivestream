b3bdec315ef9e83a7bda9e5b9a72f5a7947a3abd
iveenspace <dvienrm14@gmail.com>
2026-05-21 01:13:00 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,15 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-// This creates the webpage directly from this file
-app.get('/', (req, res) => {
-    res.send(`
-    <html>
-        <body>
-            <h1>Live Stream</h1>
-            <button id="btn">I am the Streamer</button>
-            <video id="v" autoplay playsinline style="width:500px; background:#000;"></video>
-            <script src="/socket.io/socket.io.js"></script>
-            <script>
-                const socket = io();
-                document.getElementById('btn').onclick = async () => {
-                    const stream = await navigator.mediaDevices.getDisplayMedia({video:true, audio:true});
-                    document.getElementById('v').srcObject = stream;
-                    alert("Streaming Started!");
-                };
-            </script>
-        </body>
-    </html>`);
-});
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-    console.log('User connected');
+    // Relay WebRTC signals between clients
+    socket.on('signal', (data) => {
+        socket.broadcast.emit('signal', data);
+    });
 });
 
-server.listen(3000, () => console.log('Go to http://localhost:3000'));
+http.listen(3000, () => console.log('Server running on http://localhost:3000'));
